5da325996a6aa0c4fa6b8033d1037b915f2772ff
hyunjiji0802 <guswl4174@naver.com>
2024-05-23 16:54:03 +0900

    [Gold IV] Title: 알파벳, Time: 1044 ms, Memory: 51596 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,25 +1,26 @@
-def bfs(original, x, y):
-    max_len = 1
-    q = set([(x, y, original[y][x])])  # set로 변경
-    while q:
-        x, y, alpha = q.pop()  # pop으로 변경
-        max_len = max(max_len, len(alpha))
+import sys
+r,c = map(int,sys.stdin.readline().rstrip().split())
+board = []
+for _ in range(r):
+    board.append(list(input()))
 
+    #상 하 좌 우
+dc=[0,0,-1,1]
+dr=[-1,1,0,0]
+
+def dfs(cur_c, cur_r):
+    max_len = 1
+    stack = set([(cur_c,cur_r, board[cur_r][cur_c])]) #set으로 구현 (현재 컬럼, 현재 로우, 알파벳(A~Z))을 담은 셋
+    while stack:
+        cur_c,cur_r, alpha_visited = stack.pop()
+        max_len = max(max_len, len(alpha_visited))
+        if max_len==26:
+            return 26
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < c and 0 <= ny < r and original[ny][nx] not in alpha:
-                q.add((nx, ny, alpha + original[ny][nx]))  # add로 변경
+            nc, nr = cur_c + dc[i], cur_r + dr[i]
+            if 0 <= nc < c and 0 <= nr < r and board[nr][nc] not in alpha_visited:  # 알파벳 방문리스트에 없고(처음 밟는 알파벳) 범위 벗어나지 않으면
+                stack.add((nc,nr,alpha_visited + board[nr][nc]))
 
     return max_len
 
-
-r, c = map(int, input().split())
-original = []
-for _ in range(r):
-    original.append(list(input()))
-
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-print(bfs(original, 0, 0))
+print(dfs(0,0))
