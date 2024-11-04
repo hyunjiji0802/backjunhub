b4de8c13a52685715931d0f0f2dc30abd0169b42
hyunjiji0802 <guswl4174@naver.com>
2024-11-05 01:10:11 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 6.14 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,19 @@
+import re
+
+def split_file(file):
+    tmp = re.split(r"([0-9]+)",file, maxsplit=1)
+    print(tmp)
+    return tmp
+
+def solution(files):
+    split_list =[]
+    for file in files:
+        split_list.append(split_file(file))
+
+    #split_list.sort(key = lambda x:(str(x[0]).upper(), int(re.sub(r"^0+","",x[1]))))
+    split_list.sort(key = lambda x:(str(x[0]).upper(), int(x[1])))
+    answer = []
+    for splits in split_list:
+        answer.append(''.join(splits))
+
+    return answer
