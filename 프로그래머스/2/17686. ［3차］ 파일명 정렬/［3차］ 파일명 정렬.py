e17683f168f3cc98a063a838fbcbac1f5e6484a7
hyunjiji0802 <guswl4174@naver.com>
2024-11-05 11:39:20 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.98 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -7,11 +7,13 @@ def split_file(file):
 
 def solution(files):
     split_list =[]
+    
     for file in files:
         split_list.append(split_file(file))
-
+    #x = ['img', '10', '.jpg']
+    #x[0] = 
     #split_list.sort(key = lambda x:(str(x[0]).upper(), int(re.sub(r"^0+","",x[1]))))
-    split_list.sort(key = lambda x:(str(x[0]).upper(), int(x[1])))
+    split_list.sort(key = lambda x:( x[0].upper(), int(x[1]) ))
     answer = []
     for splits in split_list:
         answer.append(''.join(splits))
