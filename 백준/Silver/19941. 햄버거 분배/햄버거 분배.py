574b9d7679a0dc1706005871889a83fe8eff05b0
hyunjiji0802 <guswl4174@naver.com>
2024-12-18 02:50:34 +0900

    [Silver III] Title: 햄버거 분배, Time: 104 ms, Memory: 111664 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,28 +1,21 @@
 from sys import stdin
 
 # 입력 받기
-N, K = map(int, stdin.readline().rstrip().split())
-arr = [1 if s == 'H' else 0 for s in stdin.readline().rstrip()]
+N, K = map(int, stdin.readline().split())
+arr = list(stdin.readline().strip())
 
-# 사람과 햄버거의 위치를 리스트로 저장
-people = [i for i in range(N) if arr[i] == 0]  # 사람의 인덱스들
-burgers = [i for i in range(N) if arr[i] == 1]  # 햄버거의 인덱스들
+# 최대 사람 수
+max_people = 0
 
-# 햄버거를 먹은 사람 수
-answer = 0
+# 왼쪽부터 오른쪽으로 탐색
+for i in range(N):
+    if arr[i] == 'P':  # 사람이 나오면
+        # 사람이 먹을 수 있는 햄버거 탐색 (왼쪽 -> 오른쪽 순서로)
+        for j in range(max(0, i - K), min(N, i + K + 1)):
+            if arr[j] == 'H':  # 햄버거를 찾으면
+                max_people += 1
+                arr[j] = 'X'  # 해당 햄버거를 사용 처리
+                break  # 한 사람은 한 개의 햄버거만 먹을 수 있으므로 탐색 종료
 
-# 햄버거를 먹을 수 있는지 확인
-burger_idx = 0  # 햄버거 리스트를 순차적으로 살펴보기 위한 포인터
-
-for person in people:
-    # 현재 사람에게 먹을 수 있는 햄버거를 찾기 위해 인덱스 점프
-    while burger_idx < len(burgers) and burgers[burger_idx] < person - K:
-        burger_idx += 1
-    
-    if burger_idx < len(burgers) and abs(burgers[burger_idx] - person) <= K:
-        # 만약 이 햄버거가 먹을 수 있다면
-        answer += 1
-        burger_idx += 1  # 이 햄버거는 더 이상 사용할 수 없으므로, 포인터 이동
-    
 # 결과 출력
-print(answer)
+print(max_people)
