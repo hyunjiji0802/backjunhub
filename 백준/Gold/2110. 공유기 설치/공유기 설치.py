a95760d32c1378649dedf32b316f42e72a62ee8b
hyunjiji0802 <guswl4174@naver.com>
2025-01-21 03:55:38 +0900

    [Gold IV] Title: 공유기 설치, Time: 240 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,28 +1,33 @@
-def count_routers(houses, distance):
-    count = 1
-    last = houses[0]
-    for house in houses[1:]:
-        if house - last >= distance:
-            count += 1
-            last = house
-    return count
+#이분탐색 - 인접한 두 공유기 사이의 최대 거리
+from sys import stdin
 
-def binary_search(houses, C, left, right):
-    if left > right:
-        return right
-    
-    mid = (left + right) // 2
-    
-    if count_routers(houses, mid) >= C:
-        return binary_search(houses, C, mid + 1, right)
+N, C = map(int, stdin.readline().rstrip().split())
+arr = []
+for _ in range(N):
+    arr.append(int(stdin.readline().rstrip()))
+
+#정렬하기
+arr.sort()
+
+def bin_search(arr, C, start, end):
+
+    if start > end:
+        return end
+
+    mid = (start + end) // 2
+    cnt = 1 #공유기 수
+    last = arr[0] #마지막으로 탐색한 집 위치
+
+    for h in arr:
+        if h - last >= mid: #거리가 더 크면
+            cnt+=1
+            last = h #업데이트
+            if cnt == C: #공유기 개수 맞으면 break
+                break
+
+    if cnt >= C:
+        return bin_search(arr, C, mid+1, end)
     else:
-        return binary_search(houses, C, left, mid - 1)
+        return bin_search(arr, C, start, mid -1)
 
-def max_distance(houses, C):
-    houses.sort()
-    return binary_search(houses, C, 1, houses[-1] - houses[0])
-
-N, C = map(int, input().split())
-houses = [int(input()) for _ in range(N)]
-
-print(max_distance(houses, C))
+print(bin_search(arr, C, 1, arr[-1] - arr[0]))
