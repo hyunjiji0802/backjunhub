4a23e2cff98222766a006c544eee9c130df6745b
hyunjiji0802 <guswl4174@naver.com>
2025-04-08 20:22:10 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,48 +1,48 @@
 # 음식종류별로 즐겨찾기수가 가장 많은 식당의 음식 종류, ID, 식당 이름, 즐겨찾기수를 조회
 # 음식 종류를 기준으로 내림차순 정렬
+# RANK() OVER (PARTITION BY <그룹화할 컬럼> ORDER BY <정렬할 컬럼> DESC)
+# WHERE 절에서는 윈도우 함수 결과 Alias 사용 X -> 서브쿼리나 CTE로
 
-# SELECT
-#     FOOD_TYPE,
-#     REST_ID,
-#     REST_NAME,
-#     FAVORITES
-# FROM REST_INFO
-
-# GROUP BY FOOD_TYPE
-# # HAVING FAVORITES = MAX(FAVORITES)
+#풀이 1. 서브쿼리
+# SELECT 
+#     tmp.FOOD_TYPE,
+#     ri.REST_ID,
+#     ri.REST_NAME,
+#     ri.FAVORITES
+# FROM REST_INFO AS ri
+# JOIN ( 
+#     SELECT
+#         FOOD_TYPE,
+#         MAX(FAVORITES) AS MAX_FAV
+#     FROM REST_INFO
+#     GROUP BY FOOD_TYPE
+# ) AS tmp
+# ON tmp.FOOD_TYPE = ri.FOOD_TYPE
+# AND tmp.MAX_FAV = ri.FAVORITES
 # ORDER BY FOOD_TYPE DESC
 
-# 음식 종류 기준으로 내림차순 정렬
-
-# SELECT
+# 풀이 2. rank()
+# 즐겨찾기 많은 순 FOOD_TYPE 별로 확인
+# SELECT 
 #     FOOD_TYPE,
 #     REST_ID,
 #     REST_NAME,
-#     FAVORITES
+#     FAVORITES,
+#     RANK() OVER (PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS r
 # FROM REST_INFO
-# ORDER BY FAVORITES DESC
-
-## 즐겨찾기가 가장 많은 식당 순으로 정렬
-# SELECT
-#     FOOD_TYPE,
-#     REST_ID,
-#     REST_NAME,
-#     FAVORITES
-# FROM REST_INFO
-# ORDER BY FOOD_TYPE DESC, FAVORITES DESC
+# ORDER BY r
 
 
 SELECT
-    RI.FOOD_TYPE,
-    RI.REST_ID,
-    RI.REST_NAME,
-    RI.FAVORITES
-FROM REST_INFO RI
-JOIN (
-    SELECT FOOD_TYPE, MAX(FAVORITES) AS MAX_FAV
+    FOOD_TYPE,
+    REST_ID,
+    REST_NAME,
+    FAVORITES
+FROM (
+    SELECT 
+        *,
+        RANK() OVER (PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS r
     FROM REST_INFO
-    GROUP BY FOOD_TYPE
-) MF
-ON RI.FOOD_TYPE = MF.FOOD_TYPE
-AND RI.FAVORITES = MF.MAX_FAV
-ORDER BY RI.FOOD_TYPE DESC;
+) AS tmp
+WHERE tmp.r = 1
+ORDER BY FOOD_TYPE DESC
