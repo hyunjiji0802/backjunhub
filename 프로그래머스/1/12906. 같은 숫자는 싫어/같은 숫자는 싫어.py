4f084015a273df6af02b2689965470d9b636c690
hyunjiji0802 <guswl4174@naver.com>
2025-06-03 17:44:31 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 160.17 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,15 +1,12 @@
 def solution(arr):
     answer = []
-    prev_num = -1 #비교할 숫자
     while arr:
-
-        pop_num = arr.pop()
-        if pop_num == prev_num : #원소가 연속되는 경우
-            pass
-            # if answer
-            # answer.append(prev_num) #이전에 추출한 원소를 answer에 담기
-        else: #다음 원소가 연속된 숫자가 아닌경우
-            answer.append(pop_num)
-        prev_num = pop_num
+        item = arr.pop()
+        
+        #answer 가 비어있거나 동일한 연속 수가 아니라면 append
+        if (len(answer) == 0) or (answer[-1] != item):
+            answer.append(item)
+            
+    #정답배열 순서 뒤집기
     answer.reverse()
     return answer
