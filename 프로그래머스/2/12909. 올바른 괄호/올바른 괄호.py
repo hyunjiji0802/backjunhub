91cf053a5a9a4ca5e4e6de1b21bc8e488c029892
hyunjiji0802 <guswl4174@naver.com>
2025-06-03 18:39:17 +0900

    [level 2] Title: 올바른 괄호, Time: 20.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,16 @@
 def solution(s):
+    answer = True
     stack = []
-    answer = False
-    for item in s:
-        if item=='(': #여는 괄호면 스택에 push
+    s = list(s)
+    #닫는 괄호면 여는괄호 만날때 까지 stack 에 담기
+    while s:
+        item = s.pop()
+        if item == ')':
             stack.append(item)
-        else: #닫는 괄호면 스택 하나 pop해서 여는 괄호 꺼내기
-            if len(stack) > 0 and stack.pop() == '(':
-                pass
-            else:
-                return False
-    if len(stack)>0: #스택이 비어있지 않으면 false
-        answer = False
-    else:
-        answer = True
-    return answer
+        elif item == '(' and len(stack)>0 and stack[-1] == ')':
+            stack.pop()
+        else:
+            return False
+        # print(s, stack, sep = "________")
+    if stack: return False
+    else: return True
