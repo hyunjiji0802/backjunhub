6bb744d98bd57d9d8dd0357a3906addfd68b9720
hyunjiji0802 <guswl4174@naver.com>
2025-06-09 18:44:40 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i,j,k = command
+        new_arr = array[i-1:j]
+        # print(new_arr)
+        new_arr.sort()
+        answer.append(new_arr[k-1])
+    return answer
