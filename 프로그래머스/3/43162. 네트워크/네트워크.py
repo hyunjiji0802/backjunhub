e569da2587a995609c32a99ad1466a25c593c56f
hyunjiji0802 <guswl4174@naver.com>
2025-06-16 23:27:59 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,25 @@
+
+def solution(n, computers):
+    answer = 0
+    #일단 모든 노드 방문 리스트
+    visited = [False for _ in range(n)]
+
+    def dfs(i):
+        #방문 체크
+        visited[i] = True
+        
+        #이웃 탐색하기
+        for j in range(n):
+            #나 말고, 다른 컴퓨터 이어져있고, 방문한 적 없으면
+            if computers[i][j] == 1 and visited[j] == False:
+                dfs(j)
+
+
+    for i in range(n):
+        if visited[i] == False:
+            #방문한 적 없으면 
+            dfs(i)
+            #네트워크 수 + 1 
+            answer+=1
+            
+    return answer
