82b15a2379639a516a605ad45ed1fcd822e239b2
hyunjiji0802 <guswl4174@naver.com>
2025-07-01 00:44:11 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 64 ms, Memory: 35060 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,30 @@
+from collections import deque
+N = int(input())
+l = deque(list(map(int, input().split())))
+
+order_stack = []
+treat_stack = []
+
+
+order_num = 1
+while l:
+    pop_num = l.popleft()
+
+    if order_num == pop_num:
+        treat_stack.append(pop_num)
+        order_num+=1
+    else:
+        order_stack.append(pop_num)
+
+    while order_stack and order_num == order_stack[-1]: 
+        treat_stack.append(order_stack.pop())
+        order_num+=1
+
+    # print(treat_stack, order_stack)
+
+sorted_stack = sorted(treat_stack + order_stack[::-1])
+
+if sorted_stack == treat_stack + order_stack[::-1]:
+    print('Nice')
+else:
+    print('Sad')
